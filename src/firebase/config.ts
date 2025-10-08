81d338b7882d735bf4f393163b961a364cd2907d
Echoes of Arjun <echoes-of-arjun@musician.org>
2025-10-08 14:21:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7986245244-8c864",
+  "appId": "1:531021681029:web:dc01d1ceca118320cc34ec",
+  "apiKey": "AIzaSyAsZsxENy_6PUj7nhj7gKGJkR3O0TEDxGE",
+  "authDomain": "studio-7986245244-8c864.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "531021681029"
+};
