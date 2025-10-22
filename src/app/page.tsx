11b60f310fc5f091345e207fe385c3b2d590a2b7
Echoes of Arjun <echoes-of-arjun@musician.org>
2025-10-22 12:06:41 +0000

    i mean the onThis project was started as a way for me, Arjun, to get my

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ import { cn } from '@/lib/utils';
 
 function ShuttlecockIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
       <path d="m6 9 6 6 6-6" /><path d="m6 9 6 6 6-6" /><path d="M12 15V21" /><path d="M8 9.5 4 6" /><path d="m16 9.5 4-3.5" /><path d="M12 9.5V6" /><path d="M10 4h4" /><path d="M8 2h8" />
     </svg>
   );
